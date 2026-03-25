a4b4f19d2a5b2b0983834e6e9a795b9faeecd3fb
etitecnologies-sketch <etitecnologies@gmail.com>
2026-03-25 20:33:16 -0300

    fix: remove accidentally pasted git commands from Finance.tsx

--- src/pages/Finance.tsx
+++ src/pages/Finance.tsx
@@ -4,14 +4,7 @@ import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { useData } from '../hooks/useData'
 import { useAuth } from '../hooks/useAuth'
-# Adicionar todas as mudanças
-git add -A
-
-# Fazer commit
-git commit -m "feat: integração 100% Receita Federal - NF-e, NFC-e, SPED, SEFAZ, Nuvemfiscal"
-
-# Push para GitHub (atualiza Vercel automaticamente)
-git push origin mainimport { useSEFAZ } from '../hooks/useSEFAZ'
+import { useSEFAZ } from '../hooks/useSEFAZ'
 import type { Transaction, FiscalDocument } from '../types'
 import FiscalDocumentModal from '../components/FiscalDocumentModal'
 import BoletoModal from '../components/BoletoModal'
